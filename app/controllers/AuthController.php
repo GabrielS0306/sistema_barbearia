c5584bf5c74fa835b9d16bdb3be3b7192d4a2297
Gabriel Santos <gabrieldossantosnunes91@gmail.com>
2026-06-18 09:37:12 -0300

    feat: Ciriação do método de logout

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -17,21 +17,28 @@
 
                     // Redireciona conforme o papel do usuário
                     match ($usuario['role']) {
-                        'admin' => header('Location: barbearia/admin/dashboard'),
-                        'barber' => header('Location: barbearia/barber/agenda'),
-                        'cliente' => header('Location: barbearia/cliente/agendamento/novo'),
-                        default => header('Location: barbearia/register') // Redireciona para o registro se o papel for desconhecido,
+                        'admin' => header('Location: /admin/dashboard'),
+                        'barber' => header('Location: /barber/agenda'),
+                        'cliente' => header('Location: /cliente/agendamento/novo'),
+                        // Redireciona para o registro se o papel for desconhecido,
+                        default => header('Location: /register')
                     };
 
                     exit;
                 }
 
                 $_SESSION['erro'] = "Email ou senha incorretos.";
-                header('Location: /barbearia/login');
+                header('Location: /login');
                 exit;
             }
 
             require __DIR__ . '/../views/auth/login.php';
+        }
+
+        public function logout(): void {
+            session_destroy();
+            header('Location: /login');
+            exit;
         }
     }
 
