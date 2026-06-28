03b183da0c4435e5b2e18c0a8681b05063e5b76c
Gabriel Santos <gabrieldossantosnunes91@gmail.com>
2026-06-28 16:51:24 -0300

    debug: testa se router.php esta sendo executado

--- public/router.php
+++ public/router.php
@@ -1,25 +1,2 @@
 <?php
-
-    // public/router.php
-    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-    // Remove prefixo /barbearia se existir (ambiente local)
-    $uri = preg_replace('#^/barbearia#', '', $uri);
-
-    // Redireciona raiz pro login
-    if ($uri === '/' || $uri === '') {
-        header('Location: /login');
-        exit;
-    }
-
-    // Se for um arquivo estático que existe, serve diretamente
-    if (file_exists(__DIR__ . $uri)) {
-        return false;
-    }
-
-    // Passa a URI pro index.php via $_GET['url']
-    $_GET['url'] = ltrim($uri, '/');
-
-    require __DIR__ . '/index.php';
-
-?>
+echo "Router funcionando! URI: " . $_SERVER['REQUEST_URI'];
