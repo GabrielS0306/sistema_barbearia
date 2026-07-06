1e3f9a489901d50f91eabed0000b6d25e880acec
Gabriel Santos <gabrieldossantosnunes91@gmail.com>
2026-07-06 15:58:50 -0300

    realterando as credebciais para a de produção

--- core/Database.php
+++ core/Database.php
@@ -10,22 +10,22 @@
 
         public static function getInstance():PDO {
             if (self::$instance === null) {
-                // Produção: variáveis de ambiente do Railway
+                // Produção: variáveis de ambiente do inift
                 // Desenvolvimento: arquivo/Database.php
                 
-                // if (getenv('MYSQLHOST')) {
-                    $host = "localhost";// getenv('MYSQLHOST');
-                    $dbname = "barbearia";// getenv('MYSQLHOST');
-                    $user = "root";// getenv('MYSQLHOST');
-                    $pass = "";// getenv('MYSQLHOST');
-                    $port = "3306";// getenv('MYSQLHOST');
+                if (getenv('MYSQLHOST')) {
+                    $host = getenv('MYSQLHOST');
+                    $dbname = getenv('MYSQLDATABASE');
+                    $user = getenv('MYSQLUSER');
+                    $pass = getenv('MYSQLPASSWORD');
+                    $port = getenv('MYSQLPORT' ?? '3306');
                     $dns = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8mb4";
-                // } else {
-                //     $cfg = require __DIR__ . '/../config/database.php';
-                //     $dns = "mysql:host={$cfg['host']};dbname={$cfg['dbname']};charset=utf8mb4";
-                //     $user = $cfg['user'];
-                //     $pass = $cfg['pass'];
-                // }
+                } else {
+                    $cfg = require __DIR__ . '/../config/database.php';
+                    $dns = "mysql:host={$cfg['host']};dbname={$cfg['dbname']};charset=utf8mb4";
+                    $user = $cfg['user'];
+                    $pass = $cfg['pass'];
+                }
 
                 self::$instance = new PDO($dns, $user, $pass, [
                     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
